a588bb02ed5a9e0ce6d0553ae926d57843669913
ArthurMN <arthurphb@alu.ufc.br>
2024-03-14 18:52:55 -0300

    Lesson 11

--- frontend/src/features/users/User.js
+++ frontend/src/features/users/User.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectUserById } from "./usersApiSlice"
 
 
